088e44bcb26725ecb6ea1c85ae2c1bff14ac010a
Tyler <tylerjamesconklin@gmail.com>
2026-08-12 19:06:41 -0500

    Update Hydra Info tab to match Malum Info tab layout exactly

--- Hydra-main/src/ui/sections/InfoSection.cs
+++ Hydra-main/src/ui/sections/InfoSection.cs
@@ -8,30 +8,29 @@ namespace HydraMenu.ui.sections
 
 		private const string MalumUrl = "https://github.com/scp222thj/MalumMenu";
 		private const string HydraUrl = "https://github.com/MrDiamond64/Hydra";
-		private const string HydralumUrl = "https://github.com/NewTabGames/Hydralum";
 
 		public override void Render()
 		{
-			GUILayout.Label("Hydralum Mod System");
-			GUILayout.Label("Pairing customized versions of MalumMenu & HydraMenu.");
+			GUILayout.Label("HydraMenu");
+			GUILayout.Label("A fork of Hydra, with features drawn from MalumMenu.");
 
-			GUILayout.Space(10);
-			GUILayout.Label("Credits & Links:");
+			GUILayout.Space(12);
+			GUILayout.Label("Credits");
 
 			DrawCredit("MalumMenu", "by scp222thj & astra1dev", MalumUrl);
-			GUILayout.Space(5);
 			DrawCredit("Hydra", "by MrDiamond64", HydraUrl);
-			GUILayout.Space(5);
-			DrawCredit("Hydralum", "by NewTabGames", HydralumUrl);
 
-			GUILayout.Space(10);
-			GUILayout.Label("Licensed under GNU General Public License v3.0.");
+			GUILayout.Space(12);
+			GUILayout.Label("MalumMenu and Hydra are both licensed under GPL-3.0.");
 		}
 
 		private static void DrawCredit(string title, string author, string url)
 		{
-			GUILayout.Label($"<b>{title}</b> ({author})");
-			if(GUILayout.Button($"Open {title} GitHub"))
+			GUILayout.Space(6);
+			GUILayout.Label($"<b>{title}</b> {author}");
+			GUILayout.Label($"<color=#9A9A9A>{url}</color>");
+
+			if (GUILayout.Button("Open GitHub", GUILayout.Width(160)))
 			{
 				Application.OpenURL(url);
 			}
