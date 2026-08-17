d576cdc8d72882c33bf6712d1e3dc41471a99cf9
Tyler <tylerjamesconklin@gmail.com>
2026-08-16 20:06:01 -0500

    Apply Keep Subwindows Open to Console and RPC Console

--- MalumMenuPlus/MalumMenu-main/src/UI/Windows/ConsoleUI.cs
+++ MalumMenuPlus/MalumMenu-main/src/UI/Windows/ConsoleUI.cs
@@ -28,7 +28,7 @@ public class ConsoleUI : MonoBehaviour
 
     private void OnGUI()
     {
-        if (!CheatToggles.showConsole || MalumMenu.isPanicked) return;
+        if (!CheatToggles.showConsole || !(MenuUI.isGUIActive || MalumMenu.menuKeepSubwindowsOpen.Value) || MalumMenu.isPanicked) return;
 
         _logStyle ??= new GUIStyle(GUI.skin.label)
         {
--- MalumMenuPlus/MalumMenu-main/src/UI/Windows/DebugUI.cs
+++ MalumMenuPlus/MalumMenu-main/src/UI/Windows/DebugUI.cs
@@ -27,7 +27,7 @@ public class DebugUI : MonoBehaviour
 
     private void OnGUI()
     {
-        if (!CheatToggles.showDebugConsole || MalumMenu.isPanicked) return;
+        if (!CheatToggles.showDebugConsole || !(MenuUI.isGUIActive || MalumMenu.menuKeepSubwindowsOpen.Value) || MalumMenu.isPanicked) return;
 
         _logStyle ??= new GUIStyle(GUI.skin.label)
         {
